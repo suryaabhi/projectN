18587e0533ee18afc1a9f266b0bc4f03616462d1
suryaabhi <surya_abhishek2@yahoo.com>
2017-08-02 00:07:03 +0530

    Added modules: Routing, Express

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { QuickSidebarComponent } from './quick-sidebar/quick-sidebar.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { QuickSidebarComponent } from './quick-sidebar/quick-sidebar.component';
     QuickSidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
