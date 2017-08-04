ecbb1507d4b370c057453fb156d6ef2d0319cbdb
suryaabhi <surya_abhishek2@yahoo.com>
2017-08-05 00:01:20 +0530

    Routing developments

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, RouterLinkActive } from '@angular/router';
 import { DashboardComponent } from '../dashboard/dashboard.component';
-import { ProfileComponent } from '../profile/profile.component'
+import { ProfileComponent } from '../profile/profile.component';
 
 const routes: Routes = [
     {
@@ -12,6 +12,10 @@ const routes: Routes = [
     {
         path: 'profile',
         component: ProfileComponent,
+    },
+    {
+        path: '**',
+        component: DashboardComponent,
     },
 ];
 
